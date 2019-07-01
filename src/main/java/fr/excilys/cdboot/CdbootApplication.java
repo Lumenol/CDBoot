d533f77068714fe105ee0448e1e68bbe2e87ea59
Louis-Maxime Crédeville <lcredeville@excilys.com>
2019-07-01 14:13:05 +0200

    Création projet

---
+++ src/main/java/fr/excilys/cdboot/CdbootApplication.java
@@ -0,0 +1,13 @@
+package fr.excilys.cdboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/excilys/cdboot/CdbootApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.excilys.cdboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CdbootApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
